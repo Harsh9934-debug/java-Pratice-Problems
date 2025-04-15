6f63dbd88e0955f6cc528823215f5578da0313af
unknown <harshkumargupta630@gmail.com>
2025-04-15 20:02:32 +0530

    Added more programs

---
+++ Type.java
@@ -0,0 +1,10 @@
+class Type{
+    public static void main(String args[])
+    {
+        int a = 10;
+        float f =20.123f;
+        int c = (int)f;
+        System.out.println("float value is: " + f);
+        System.out.println("int value after casting is: " + c);
+    }
+}
---
+++ Typrcasting.java
@@ -0,0 +1,11 @@
+class Typrcasting
+{
+    public static void main(String args[])
+    {
+        int a = 10;
+        float f = 20.5f;
+        int b = (int) f; 
+        System.out.println("float value is: " + f);
+        System.out.println("int value after casting is: " + b);
+    }
+}
