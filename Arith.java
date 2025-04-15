ba7f6f18181f9deb04e308dc7d5f18b8a1035280
unknown <harshkumargupta630@gmail.com>
2025-04-15 20:12:34 +0530

    Aded more

---
+++ Arith.java
@@ -0,0 +1,9 @@
+class Arith{
+    public static void main(String args[]){
+        int a = 10;
+        float f =20.234f;
+        // int b = (int)f;
+        int  d= a + (int)f;
+        System.out.println("Added value is"+d);
+    }
+}
